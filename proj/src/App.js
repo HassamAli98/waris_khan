61cb6a2288477c61329eccf651abf9f9b318b7fc
Waris khan <waris@gmail.com>
2024-05-25 13:19:28 +0500

    init

---
+++ proj/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className='flex'>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt saepe similique consectetur aspernatur modi repudiandae nobis. Adipisci, nemo! Vitae repellendus dolor accusantium, tempora vel delectus repellat facilis numquam odit ullam.</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt saepe similique consectetur aspernatur modi repudiandae nobis. Adipisci, nemo! Vitae repellendus dolor accusantium, tempora vel delectus repellat facilis numquam odit ullam.</div>
+    </div>
+  );
+}
+
+export default App;
